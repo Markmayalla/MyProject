27308be86e00f774033de71a77c681188901e085
mark p <markmayalla@gmail.com>
2018-11-05 22:25:47 +0300

    Initial Project Setup

---
+++ app/src/main/java/tz/co/comptech/m_safariproduction/MainActivity.java
@@ -0,0 +1,13 @@
+package tz.co.comptech.m_safariproduction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
